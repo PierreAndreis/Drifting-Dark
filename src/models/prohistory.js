a93ec472b1878de81d8d9212c339a11aa59f74cd
Skillz <gankstarsleague@gmail.com>
2017-10-25 02:41:07 -0400

    linter

--- src/models/prohistory.js
+++ src/models/prohistory.js
@@ -1,43 +1,45 @@
 import allPlayers from "../resources/vg8_teams/teams_autumn_1";
 import matches from "./vg_matches";
 
-const players = {}
-const counter = 0;
+const players = {};
+let counter = 0;
 
 // Run a loop depending on how many teams there are
 for (let i = 0; i < allPlayers.length; i++) {
-    // For each player in the team add their IGN and region to players
-    players[i].ign = allPlayers[i].players.name
-    players[i].region = allPlayers[i].players.region
+  // For each player in the team add their IGN and region to players
+  players[i].ign = allPlayers[i].players.name;
+  players[i].region = allPlayers[i].players.region;
 }
-// Infinite loop every 1 minute calling the queuePros function
-setInterval(queuePros(), 60000);
 
 async function queuePros() {
-    let maxSearch = counter + 15
-    if (!players.length > maxSearch) {
-        // If the max loop counter is more than players.length decrease the maxSearch 
-        const difference = maxSearch - players.length
-        maxSearch -= difference
-    }
-    // Loop through the list of players starting from the counter and for 15 players
+  let maxSearch = counter + 15;
+  if (!players.length > maxSearch) {
+    // If the max loop counter is more than players.length decrease the maxSearch
+    const difference = maxSearch - players.length;
+    maxSearch -= difference;
+  }
+  // Loop through the list of players starting from the counter and for 15 players
   for (let i = counter; i < maxSearch; i++) {
-    let lastMatch
+    let lastMatch;
     // If the lastMatch exists use it
     if (players[i].lastMatch) {
-        lastMatch = players[i].lastMatch;
+      lastMatch = players[i].lastMatch;
     } else {
-        // if the lastMatch didnt exist create it for NOW
-        lastMatch = new Date().toISOString();
-        // Add lastMatch to the player for next time
-        players[i].lastMatch = lastMatch
+      // if the lastMatch didnt exist create it for NOW
+      lastMatch = new Date().toISOString();
+      // Add lastMatch to the player for next time
+      players[i].lastMatch = lastMatch;
     }
-    // If the counter is at the last player reset to 0 else add 1 to the counter 
-    if (counter === player.length) {
-        counter = 0
-    } else counter++
-    const data = await matches.getMatches(players[i].ign, players[i].region, lastMatch);
+    // If the counter is at the last player reset to 0 else add 1 to the counter
+    if (counter === players.length) {
+      counter = 0;
+    } else counter++;
+    const data = matches.getMatches(players[i].ign, players[i].region, lastMatch);
     // If error move to next loop
-    if (!data) continue
+    if (!data) continue;
     // TODO: Store the data in redis
+  }
 }
+
+// Infinite loop every 1 minute calling the queuePros function
+setInterval(queuePros(), 60000);
