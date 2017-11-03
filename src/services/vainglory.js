a934f4ad156f76951bddd66739e19fa2b0a4a026
Skillz <gankstarsleague@gmail.com>
2017-11-02 23:38:11 -0400

    fixing some errorss

--- src/services/vainglory.js
+++ src/services/vainglory.js
@@ -8,7 +8,6 @@ const RESULT_PER_PAGE = 50;
  * todoschema,
  * verify region
  */
-
 const generateOpt = (options) => lodash.defaultsDeep(
     Config.VAINGLORY.DEFAULT_OPTION, 
     {filter: options, page: {limit: RESULT_PER_PAGE}}
@@ -32,17 +31,19 @@ class VaingloryService {
       "createdAt-end": lastMatch,
     };
     if (gameMode) options.gameMode = gameMode;
+    const patchData = [];
     if (patch) {
       patch = patch.sort((a, b) => {
         if (a > b) return 1;
         if (b < a) return -1;
         return 0;
       });
-        const patches = Config.VAINGLORY.PATCH_DATES.keys();
+        const patches = Object.keys(Config.VAINGLORY.PATCH_DATES);
         patch.forEach((r) => {
-          options["createdAt-start"] = Date.parse(Config.VAINGLORY.PATCH_DATES[r]).toISOString();
+          options["createdAt-start"] = new Date(Date.parse(Config.VAINGLORY.PATCH_DATES[r])).toISOString();
           const index = patches.indexOf(r) + 1;
-          options["createdAt-end"] = Date.parse(Config.VAINGLORY.PATCH_DATES[index]).toISOString();
+          console.log(r)
+          options["createdAt-end"] = new Date(Date.parse(Config.VAINGLORY.PATCH_DATES[patches[index]])).toISOString();
           options = generateOpt(options)
           if (page) options.page = { offset: RESULT_PER_PAGE * page };
           patchData.push(this.queryMatches(region, options))
