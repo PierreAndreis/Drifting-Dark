9af9f0fe5fc2b5d72d178442096146c3c85c1f9c
Skillz <gankstarsleague@gmail.com>
2017-11-21 15:32:47 -0500

    moving to separate logger file

--- src/index.js
+++ src/index.js
@@ -4,31 +4,3 @@ import winston from "winston"
 const {PORT = 8080} = process.env;
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`)); // eslint-disable-line no-console
-
-const logger = winston.createLogger({
-    level: 'silly',
-    format: winston.format.json(),
-    transports: [
-      //
-      // - Write to all logs with level `silly` and below to `combined.log` 
-      // - Write all logs error (and below) to `error.log`.
-      //
-      new winston.transports.File({ filename: 'error.log', level: 'error' }),
-      new winston.transports.File({ filename: 'combined.log' })
-    ]
-  });
-  
-//
-// If we're not in production then log to the `console` with the format:
-// `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
-// 
-if (process.env.NODE_ENV !== 'production') {
-logger.add(new winston.transports.Console({
-    format: winston.format.simple()
-}));
-}
-
-logger.info('test info')
-logger.error('test error')
-logger.debug('test debug')
-
--- src/lib/logger.js
+++ src/lib/logger.js
@@ -1,1 +1,22 @@
-// todo 
+const logger = winston.createLogger({
+    level: 'silly',
+    format: winston.format.json(),
+    transports: [
+      //
+      // - Write to all logs with level `silly` and below to `combined.log` 
+      // - Write all logs error (and below) to `error.log`.
+      //
+      new winston.transports.File({ filename: 'error.log', level: 'error' }),
+      new winston.transports.File({ filename: 'combined.log' })
+    ]
+  });
+  
+//
+// If we're not in production then log to the `console` with the format:
+// `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
+// 
+if (process.env.NODE_ENV !== 'production') {
+logger.add(new winston.transports.Console({
+    format: winston.format.simple()
+}));
+}
