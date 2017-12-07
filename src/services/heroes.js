8f2b92e96f6202452029bf791ac5ae92ae8cf963
Skillz <gankstarsleague@gmail.com>
2017-12-07 01:54:02 -0500

    adding telem to heroes

--- src/services/heroes.js
+++ src/services/heroes.js
@@ -1,13 +1,33 @@
-import { generateHeroesStats } from "~/transforms/global_heroes.js";
-import 
+import { generateHeroesStats }  from "~/transforms/global_heroes.js";
+import Telemetry                from "~/transforms/telemetry.js";
+import MatchesModel             from "~/models/vg_matches.js";
 
 async function heroStats(match) {
   const heroes = {};
 
   for (const player of match.players) {
-    heroes[player.actor] = [generateHeroesStats(match, player)];
+    heroes[player.actor] = generateHeroesStats(match, player);
   }
-  const telem =
+  const telem = await MatchesModel.getMatchTelemetry(match.telemetry.URL, match.id);
+
+  for (const pick of telem.Draft) {
+    if (pick.Type !== "HeroBan") continue;
+    heroes[pick.Hero] = {
+      bans: 1,
+    };
+  }
+  const facts = telem.Facts;
+  const factKeys = Object.keys(facts);
+  for (const team of factKeys) {
+    const teamKeys = Object.keys(facts[team]);
+    for (const hero of teamKeys) {
+      const actor = facts[team][hero];
+      heroes[hero].TotalHealed = actor.TotalHealed;
+      heroes[hero].TotalDealt = actor.TotalDealt;
+      heroes[hero].abilityPattern = actor.Skill;
+    }
+  }
+
   return heroes;
 }
 
