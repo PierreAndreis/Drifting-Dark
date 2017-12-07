e7fee50c33e9472a2e805c18d153e0e353527d2b
Skillz <gankstarsleague@gmail.com>
2017-12-06 23:04:35 -0500

    adding synergy and counters

--- src/services/heroes.js
+++ src/services/heroes.js
@@ -6,22 +6,24 @@ async function heroStats(match) {
   for (const player of match.players) {
     const stats = generateHeroesStats(match, player);
     heroes[player.actor] = stats;
+    heroes[player.actor].wonWith = [];
+    heroes[player.actor].wonAgainst = [];
+    heroes[player.actor].lostWith = [];
+    heroes[player.actor].lostAgainst = [];
+    if (stats.wins) {
+      for (const winner of match.players) {
+        if (winner === player) continue;
+        if (winner.side === player.side) heroes[winner.actor].wonWith.push(winner.actor);
+        if (winner.side !== player.side) heroes[winner.actor].wonAgainst.push(winner.actor);
+      }
+    } else {
+      for (const loser of players) {
+        if (loser === player) continue;
+        if (loser.side === player.side) heroes[loser.actor].lostWith.push(loser.actor);
+        if (loser.side !== player.side) heroes[loser.actor].lostAgainst.push(loser.actor);
+      }
+    }
   }
-
-  // TODO: wonWith, lostWith, wonAgainst, lostAgainst to make synergy and counter stats
-  // if (heroes[actor].wins) {
-  //   for (const winner of players) {
-  //     if (winner === players[i]) continue;
-  //     if (winner.side === players[i].side) heroes[winner.actor].wonWith.push(winner.actor);
-  //     if (winner.side !== players[i].side) heroes[winner.actor].wonAgainst.push(winner.actor);
-  //   }
-  // } else {
-  //   for (const loser of players) {
-  //     if (loser === players[i]) continue;
-  //     if (loser.side === players[i].side) heroes[loser.actor].lostWith.push(loser.actor);
-  //     if (loser.side !== players[i].side) heroes[loser.actor].lostAgainst.push(loser.actor);
-  //   }
-  // }
   return heroes;
 }
 
