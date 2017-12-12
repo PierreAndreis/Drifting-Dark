02eb39fa1cd5fe805a6d3e5d84f6b1528cc7d835
Skillz <gankstarsleague@gmail.com>
2017-12-12 15:45:30 -0500

    removing console logs

--- src/routers/leaderboards.router.js
+++ src/routers/leaderboards.router.js
@@ -4,13 +4,10 @@ import Redis from "~/services/redis.js";
 
 // leaderboards/:name
 export const vpr = async (req, res, next) => {
-    console.log('this ran')
   const { name } = req.params;
   const { patch, gameMode, page } = req.query;
   let reply = await MatchesController.getMatchesByName(name, { patch, gameMode, page });
   reply = await VPR.update(reply[0]);
-  console.log(reply)
-  const vprs = [];
   for (const player of reply.players) {
     const vpr = player.vpr;
     if (!vpr.won) vpr.amount = -Math.abs(vpr.amount);
