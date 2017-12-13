97dd187f0b7288240195a146355a77e2cd28d59d
Skillz <gankstarsleague@gmail.com>
2017-12-12 21:42:07 -0500

    small error

--- src/routers/leaderboards.router.js
+++ src/routers/leaderboards.router.js
@@ -2,8 +2,9 @@ import Redis from "~/services/redis.js";
 
 // leaderboards/:name
 export const vpr = async (req, res, next) => {
-  const { region, amount } = req.query;
+  const { region, amount, type } = req.query;
   const limit = amount || 100;
+  const table =
   const leaderboard = await Redis.zrevrange(`vpr:${region}`, 0, limit, 'withscores');
   res.json(leaderboard);
 };
--- src/services/leaderboards.js
+++ src/services/leaderboards.js
@@ -1,16 +1,16 @@
 import Redis from "~/services/redis.js";
 
 class Leaderboards {
-    async vpr(table, max, min = 0) {
-        return Redis.zrevrange(`vpr:${table}`, min, max, 'withscores');
+    async vpr(table, region, max, min = 0) {
+        return Redis.zrevrange(`vpr:${region}`, min, max, 'withscores');
     }
 
-    async vst(table, max, min = 0) {
-        return Redis.zrevrange(`vst:${table}`, min, max, 'withscores');
+    async vst(table, region, max, min = 0) {
+        return Redis.zrevrange(`vst:${region}`, min, max, 'withscores');
     }
 
-    async heroes(table, max, min = 0) {
-        return Redis.zrevrange(`heroes:${table}`, min, max, 'withscores');
+    async heroes(table, region, max, min = 0) {
+        return Redis.zrevrange(`heroes:${region}`, min, max, 'withscores');
     }
 }
 
