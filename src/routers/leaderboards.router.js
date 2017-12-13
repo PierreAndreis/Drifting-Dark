f3ff4dfe64dc11709f9ce2478c6cf642161f97aa
Skillz <gankstarsleague@gmail.com>
2017-12-12 21:20:33 -0500

    small error

--- src/routers/leaderboards.router.js
+++ src/routers/leaderboards.router.js
@@ -2,7 +2,6 @@ import Redis from "~/services/redis.js";
 
 // leaderboards/:name
 export const vpr = async (req, res, next) => {
-  const { name } = req.params;
   const { region, amount } = req.query;
   const limit = amount || 100;
   const leaderboard = await Redis.zrevrange(`vpr:${region}`, 0, limit, 'withscores');
