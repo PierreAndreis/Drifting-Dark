5ccc99aaa034fb57cba586b94a228fa24acfd010
Skillz <gankstarsleague@gmail.com>
2017-12-12 21:45:28 -0500

    more flexibility

--- src/routers/leaderboards.router.js
+++ src/routers/leaderboards.router.js
@@ -2,9 +2,10 @@ import Leaderboards from "~/services/leaderboards.js";
 
 // leaderboards/:name
 export const vpr = async (req, res, next) => {
-  const { region, amount, type } = req.query;
-  const limit = amount || 100;
-  const leaderboard = await Leaderboards[type](region, 0, limit, 'withscores');
+  const { region, min, max, type } = req.query;
+  const limit = max || 100;
+  const start = min || 0;
+  const leaderboard = await Leaderboards[type](region, start, limit, 'withscores');
   res.json(leaderboard);
 };
 
