d6418572d02ebed3fdecaa965e0e5d03b7881d68
Skillz <gankstarsleague@gmail.com>
2017-12-12 21:44:20 -0500

    fixing the import

--- src/routers/leaderboards.router.js
+++ src/routers/leaderboards.router.js
@@ -1,10 +1,10 @@
-import Redis from "~/services/redis.js";
+import Leaderboards from "~/services/leaderboards.js";
 
 // leaderboards/:name
 export const vpr = async (req, res, next) => {
   const { region, amount, type } = req.query;
   const limit = amount || 100;
-  const leaderboard = await Redis.zrevrange(type, region, 0, limit, 'withscores');
+  const leaderboard = await Leaderboards[type](region, 0, limit, 'withscores');
   res.json(leaderboard);
 };
 
--- src/services/leaderboards.js
+++ src/services/leaderboards.js
@@ -1,15 +1,15 @@
 import Redis from "~/services/redis.js";
 
 class Leaderboards {
-    async vpr(table, region, max, min = 0) {
+    vpr(region, max, min = 0) {
         return Redis.zrevrange(`vpr:${region}`, min, max, 'withscores');
     }
 
-    async vst(table, region, max, min = 0) {
+    vst(region, max, min = 0) {
         return Redis.zrevrange(`vst:${region}`, min, max, 'withscores');
     }
 
-    async heroes(table, region, max, min = 0) {
+    heroes(region, max, min = 0) {
         return Redis.zrevrange(`heroes:${region}`, min, max, 'withscores');
     }
 }
