016bd059d5d00aaec350269a095bfe3e3d830515
Skillz <gankstarsleague@gmail.com>
2017-12-24 21:20:34 -0500

    refactoring

--- src/models/vg_heroes.js
+++ src/models/vg_heroes.js
@@ -11,9 +11,8 @@ const HEROESDB = new CouchbaseService("heroes");
 const cacheKey = "HeroesStats";
 
 class VGHeroes extends BaseCouchbase {
-
   callCouch(select, from, where, group, order, ascDesc) {
-    console.log('inside coudh')
+    console.log("inside coudh");
     if (!select) return this.query("SELECT patchVersion, actor, SUM(bans) as bans, SUM(games) as games FROM heroes WHERE patchVersion = '2.11' GROUP BY patchVersion, actor ORDER BY bans DESC");
     return this.query(`SELECT ${select} FROM ${from} WHERE ${where} GROUP BY ${group} ORDER BY ${order} ${ascDesc}"`);
   }
@@ -22,32 +21,57 @@ class VGHeroes extends BaseCouchbase {
     return dbData.reduce((prev, now) => prev.games + now.games) / 6;
   }
 
-  async bans() {
+  async heroData(type) {
     const dbData = await this.callCouch();
-    console.log(dbData)
     return dbData.map((hero) => {
-      console.log('inside map')
-      hero.banRate    = hero.bans / this.getTotalGames(dbData);
-      console.log(hero.banRate)
+      let numerator = "";
+      let property  = "";
+      switch (type) {
+        case "bans":
+          numerator   = hero.bans;
+          property    = "banRate";
+          break;
+        case "picks":
+          numerator = hero.games - hero.bans;
+          property    = "pickRate";
+          break;
+        case "games":
+          numerator   = hero.games;
+          property    = "gamesRate";
+          break;
+        default:
+      }
+      hero[property]  = numerator / this.getTotalGames(dbData);
       return hero;
     });
   }
 
-  async picks() {
-    const dbData = await this.callCouch();
-    return dbData.map((hero) => {
-      hero.pickRate   = (hero.games - hero.bans) / this.getTotalGames(dbData);
-      return hero;
-    });
-  }
-
-  async games() {
-    const dbData = await this.callCouch();
-    return dbData.map((hero) => {
-      hero.gamesRate  = hero.games / this.getTotalGames(dbData);
-      return hero;
-    });
-  }
+  // async bans() {
+  //   const dbData = await this.callCouch();
+  //   console.log(dbData);
+  //   return dbData.map((hero) => {
+  //     console.log("inside map");
+  //     hero.banRate    = hero.bans / this.getTotalGames(dbData);
+  //     console.log(hero.banRate);
+  //     return hero;
+  //   });
+  // }
+  //
+  // async picks() {
+  //   const dbData = await this.callCouch();
+  //   return dbData.map((hero) => {
+  //     hero.pickRate   = (hero.games - hero.bans) / this.getTotalGames(dbData);
+  //     return hero;
+  //   });
+  // }
+  //
+  // async games() {
+  //   const dbData = await this.callCouch();
+  //   return dbData.map((hero) => {
+  //     hero.gamesRate  = hero.games / this.getTotalGames(dbData);
+  //     return hero;
+  //   });
+  // }
 }
 
 export default new VGHeroes(HEROESDB);
