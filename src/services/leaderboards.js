b3c7f9f7691de70384e60dbd1729e552d48783f2
4ever <pierreandreisortega@gmail.com>
2018-01-06 23:09:58 -0500

    Reverting the revert

--- src/services/leaderboards.js
+++ src/services/leaderboards.js
@@ -1,21 +1,25 @@
 import Redis from "~/services/redis.js";
 
+const key = "VGPRO:Leaderboard:";
+
 class Leaderboards {
-  constructor(name) {
-    this.name = name;
+  constructor(type, name, region) {
+    this.name = `${key}:${type}:${name}:${region}`;
   }
-
   get(playerId) {
-    return Redis.zrank(this.name, playerId);
+    return Redis.zrevrank(this.name, playerId);
   }
-
   set(playerId, score) {
     return Redis.zadd(this.name, score, playerId);
   }
-
   range(min, max) {
     return Redis.zrevrange(this.name, min, max);
   }
-}
 
-export default new Leaderboards();
+  async updateAndGet(playerId, score) {
+    const res = await this.set(playerId, score);
+    return await this.get(playerId) + 1;
+  }
+};
+
+export default Leaderboards;
