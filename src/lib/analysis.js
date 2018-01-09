05a2bca82d16d1792f8130f5e341e6b3617c00b2
4ever <pierreandreisortega@gmail.com>
2018-01-08 23:46:15 -0800

    Oops, forgetting

--- src/lib/analysis.js
+++ src/lib/analysis.js
@@ -4,24 +4,24 @@ import logger     from "./logger";
 const dogstatsd = new StatsD();
 
 const Analysis = {
-  increment(key) {
-    return dogstatsd.increment(key);
+  increment(key, tags) {
+    return dogstatsd.increment(key, tags);
   },
 
-  incrementBy(key, value) {
-    return dogstatsd.incrementBy(key, value)
+  incrementBy(key, value, tags) {
+    return dogstatsd.incrementBy(key, value, tags);
   },
 
-  decrement(key) {
-    return dogstatsd.decrement(key);
+  decrement(key, tags) {
+    return dogstatsd.decrement(key, tags);
   },
 
-  decrementBy(key, value) {
-    return dogstatsd.decrementBy(key, value);
+  decrementBy(key, value, tags) {
+    return dogstatsd.decrementBy(key, value, tags);
   },
 
-  timing(key, time) {
-    return dogstatsd.timing(key, time);
+  timing(key, time, tags) {
+    return dogstatsd.timing(key, time, tags);
   }
 
 }
