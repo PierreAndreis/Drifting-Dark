af6aa62ea999ed8e0f238d97e0d757676f72d237
Skillz <gankstarsleague@gmail.com>
2018-01-22 19:05:54 -0500

    added octokit for cron and router set to redis

--- src/routers/wikis.router.js
+++ src/routers/wikis.router.js
@@ -1,10 +1,11 @@
 import Showdown from "showdown";
+import RedisCache   from "~/services/cache";
 
 /* /wiki/:type/:section */
 export const wiki = async (req, res, next) => {
   const { type, section } = req.params;
   const converter         = Showdown.Converter();
-  const md                = ''; // todo: get the md file
-  const reply = await converter.makeHtml(md);
+  const md                = await RedisCache.get(`${type}/${section}`);
+  const reply             = await converter.makeHtml(md);
   res.json(reply);
 };
