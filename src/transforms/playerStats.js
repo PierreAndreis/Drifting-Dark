b1610e672bae26c955410693df5bbd407c1bd348
Pierre Ortega <pierreandreisortega@gmail.com>
2018-02-05 22:10:04 -0800

    Enabling Roles on Casual 5v5

--- src/transforms/playerStats.js
+++ src/transforms/playerStats.js
@@ -136,8 +136,8 @@ class PlayerStatsInput {
 
     const role = player.role;
 
-    // Role shouldn't be a thing on anything not Ranked or Casual
-    if (match.gameMode !== "Ranked" && match.gameMode !== "Casual") return false;
+    // Role shouldn't be a thing on anything not Ranked or Casual 
+    if (match.gameMode !== "Ranked" && match.gameMode !== "Casual" && match.gameMode == "Casual 5v5") return false;
 
     return {
       [match.gameMode]: {
