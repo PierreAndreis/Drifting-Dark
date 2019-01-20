68765b6c5fcbb079332975f06b23d45238081b53
Pierre Ortega <pierreandreisortega@gmail.com>
2019-01-19 20:25:52 -0800

    disable vgprime

--- src/services/vgprime.js
+++ src/services/vgprime.js
@@ -3,15 +3,18 @@ import fetch from "node-fetch";
 // VERY BAD HARD CODED BELOW
 // Dont care.
 export default matches => {
-  return matches.forEach(match =>
-    fetch("https://services.vgpro.gg/input", {
-      method: "POST",
-      body: JSON.stringify(match),
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: "Basic dG9rZW46amVhbi1saW5kbw=="
-      }
-    })
-      .catch(err => console.warn("FAILED TO SEND MATCH TO VGPRIME=", err))
-  );
+  return;
+
+  /** VGPRIME is down */
+  // return matches.forEach(match =>
+  //   fetch("https://services.vgpro.gg/input", {
+  //     method: "POST",
+  //     body: JSON.stringify(match),
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //       Authorization: "Basic dG9rZW46amVhbi1saW5kbw=="
+  //     }
+  //   })
+  //     .catch(err => console.warn("FAILED TO SEND MATCH TO VGPRIME=", err))
+  // );
 };
